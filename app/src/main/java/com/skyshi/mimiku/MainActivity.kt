71eb197d80c8c1dff5d9c9036ae125c54b352008
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-07-19 14:11:41 +0700

    Add preference to save time

--- app/src/main/java/com/skyshi/mimiku/MainActivity.kt
+++ app/src/main/java/com/skyshi/mimiku/MainActivity.kt
@@ -1,29 +1,47 @@
 package com.skyshi.mimiku
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedpreferences = getSharedPreferences("mimiku", Context.MODE_PRIVATE) ?: return
-        setText(sharedpreferences.getInt("counter", 0))
+        sharedPreferences = getSharedPreferences("mimiku", Context.MODE_PRIVATE) ?: return
+
+        val c = Calendar.getInstance()
+        c.timeInMillis = sharedPreferences.getLong("time", 0)
+        c.get(Calendar.DAY_OF_MONTH)
+
+        if (Calendar.getInstance().get(Calendar.DAY_OF_MONTH) != c.get(Calendar.DAY_OF_MONTH)) {
+            setText(0)
+        } else {
+            setText(sharedPreferences.getInt("counter", 0))
+        }
+
 
         frame_layout.setOnClickListener {
-            val x = sharedpreferences.edit()
-            x.putInt("counter", sharedpreferences.getInt("counter", 0) + 1)
-            setText(sharedpreferences.getInt("counter", 0) + 1)
-            x.apply()
+            setText(sharedPreferences.getInt("counter", 0) + 1)
+
         }
+
     }
 
     private fun setText(y: Int) {
+        val x = sharedPreferences.edit()
+        x.putInt("counter", y)
+        x.putLong("time", Calendar.getInstance().timeInMillis)
         text_view.text = y.toString()
+        x.apply()
     }
 }
